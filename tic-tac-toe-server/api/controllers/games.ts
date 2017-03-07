df1f359652e07d5191cd241d6374df0500035fb8
Chris Cotar <christopher.cotar@blackbaud.com>
2017-03-07 15:20:30 -0600

    day1 progress

--- tic-tac-toe-server/api/controllers/games.ts
+++ tic-tac-toe-server/api/controllers/games.ts
@@ -1,12 +1,34 @@
 import * as express from 'express';
 import { SwaggerRequest } from './swaggerInterop';
+import { GameRepository } from '../models/gameRepository';
+
+let repo = new GameRepository();
 
 export function list(req: express.Request, res: express.Response) : void {
+  let game = {
+    id: 1,
+    startedOn: Date.now().toString(),
+    humanPlayerFirst: true,
+    board: [],
+    completed: {
+      winnerIndex: 1
+    }
+  };
+  let games = [game];
+  let gameListResponse = {
+    games: games
+  };
+  console.log(gameListResponse);
+  res.json(gameListResponse);
 
 };
 
 export function create(req: SwaggerRequest, res: express.Response) : void {
   let command = req.swagger.params.gameRequest.value;
+
+  let { humanPlayerFirst } = req.swagger.params.gameRequest.value;
+  let newGame = repo.addGame(humanPlayerFirst);
+  res.status(201).json(newGame);
 };
 
 export function details(req: SwaggerRequest, res: express.Response) : void {
